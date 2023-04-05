f16c92c6405bd59e9196071202fcdb6e5547b700
Mateusz Nawieśniak <mateusz.nawiesniak@gmail.com>
2023-04-05 14:46:21 +0200

    Added automapper

--- RowerOwO/Database/AutoMapperProfile.cs
+++ RowerOwO/Database/AutoMapperProfile.cs
@@ -9,10 +9,19 @@ namespace RowerOwO.Database
         public AutoMapperProfile() 
         {
             CreateMap<VehicleCreateViewModel, VehicleModel>();
+            CreateMap<VehicleModel,VehicleCreateViewModel>();
+
             CreateMap<VehicleEditViewModel, VehicleModel>();
+            CreateMap<VehicleModel, VehicleEditViewModel>();
+
             CreateMap<VehicleDetailsViewModel, VehicleModel>();
+            CreateMap<VehicleModel, VehicleDetailsViewModel>();
+
             CreateMap<VehicleListViewModel, VehicleModel>();
+            CreateMap<VehicleModel, VehicleListViewModel>();
+
             CreateMap<RentalPointCRUDViewModel, RentalPointModel>();
+            CreateMap<RentalPointModel,RentalPointCRUDViewModel>();
         }
     }
 }
