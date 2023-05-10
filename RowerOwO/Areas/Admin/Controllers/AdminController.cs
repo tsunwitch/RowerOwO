4d3850cdec614deb32bff9151040b081041c6ae8
Mateusz Nawieśniak <mateusz.nawiesniak@gmail.com>
2023-05-10 16:40:11 +0200

    Added user display
    yahooo

--- RowerOwO/Areas/Admin/Controllers/AdminController.cs
+++ RowerOwO/Areas/Admin/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using RowerOwO.Areas.Admin.ViewModels;
 using System.Data;
 
 namespace RowerOwO.Areas.Admin.Controllers
@@ -8,9 +11,29 @@ namespace RowerOwO.Areas.Admin.Controllers
 	[Authorize(Roles = "Administrator")]
 	public class AdminController : Controller
 	{
+		private readonly UserManager<IdentityUser> usermgr;
+
+		public AdminController(UserManager<IdentityUser> usermgr)
+		{
+			this.usermgr = usermgr;
+		}
+
 		public IActionResult Index()
 		{
-			return View();
+			var userList = usermgr.Users.ToList();
+			var viewModelUserList = new List<UserListViewModel>();
+
+			foreach(var user in userList)
+			{
+				viewModelUserList.Add(new UserListViewModel
+				{
+					Id = user.Id.ToString(),
+					Name = user.UserName,
+					Email = user.Email
+				});
+			}
+
+			return View(viewModelUserList);
 		}
 
 		public IActionResult Dupa()
---
+++ RowerOwO/Areas/Admin/ViewModels/UserListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RowerOwO.Areas.Admin.ViewModels
+{
+	public class UserListViewModel
+	{
+		public string Id { get; set; }
+		public string? Name { get; set; }
+		public string? Email { get; set; }
+		public IdentityRole[]? Roles { get; set; }
+	}
+}
