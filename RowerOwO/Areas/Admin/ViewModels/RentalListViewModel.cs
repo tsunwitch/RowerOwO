637a7c9438b406046f959b2335827db7b0a913ca
Mateusz Nawieśniak <mateusz.nawiesniak@gmail.com>
2023-05-29 16:04:08 +0200

    Fixed some styling issues

--- RowerOwO/Areas/Admin/ViewModels/RentalListViewModel.cs
+++ RowerOwO/Areas/Admin/ViewModels/RentalListViewModel.cs
@@ -6,6 +6,7 @@ namespace RowerOwO.Areas.Admin.ViewModels
     {
         public Guid Id { get; set; }
         public virtual VehicleModel? Vehicle { get; set; }
+        public virtual RentalPointModel? RentalPoint { get; set; }
         public DateOnly? RentFrom { get; set; }
         public DateOnly? RentTill { get; set; }
     }
