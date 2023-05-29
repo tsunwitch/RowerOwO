ec6c60e9c43d257ef3b771673621ad16ee29664c
Mateusz Nawieśniak <mateusz.nawiesniak@gmail.com>
2023-05-29 17:12:12 +0200

    Changed form type to work with HttpPost method

--- RowerOwO/Areas/Admin/Controllers/AdminController.cs
+++ RowerOwO/Areas/Admin/Controllers/AdminController.cs
@@ -120,6 +120,12 @@ namespace RowerOwO.Areas.Admin.Controllers
             return RedirectToAction("Roles");
         }
 
+		[HttpPost]
+		public IActionResult EditRentalSubmit(Guid id, string vehicleName, string rentalPointName, DateOnly rentFrom, DateOnly rentTill)
+		{
+			return RedirectToAction("Rentals");
+		}
+
 		[HttpGet]
         public IActionResult DeleteRental(Guid id)
         {
